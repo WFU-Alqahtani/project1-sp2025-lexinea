3945184bfdf37f43d9f697b84254bcd37b88973e
lexineappiah <appily23@wfu.edu>
2025-01-24 15:59:14 -0500

    Created an array called games for the store inventory.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        String[] games = {"Mario Kart 8", "Donkey Kong", "Pac-Man", "Splatoon 2", "Kirby's Dreamland"};
+        ArrayList<String> gameList = new ArrayList<String>();
         System.out.println("fix me!");
+
     }
 
     //Creating the setupStore method
-    public static String[] setupStore () {
-
+    public static String[] setupStore (String[] a) {
+        return a;
     }
+
+    //Creating the
 
 }
