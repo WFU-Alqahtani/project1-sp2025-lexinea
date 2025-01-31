21c7b4baded156fa6bfdf2151aa307855ac80123
lexineappiah <appily23@wfu.edu>
2025-01-30 19:36:35 -0500

    Fixed the setupStore method; I realized that the "String" needed to be replaced with "Item" for the Item array.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         //String[] games = {"Mario Kart 8", "Donkey Kong", "Pac-Man", "Splatoon 2", "Kirby's Dreamland"};
        // String[] items = setupStore(store); //method call for setupStore
+       //creating Item array for the setupStore method
+       Item[] store = new Item[5];
+
         ArrayList<String> gameList = new ArrayList<String>();
 
         //adding the games into the gameList
@@ -19,14 +22,15 @@ public class Main {
 
     }
 
-    //Creating the setupStore method to return the array of list of games
-    public static String[] setupStore () {
-        String[] store = new String [5];
-        store[0] = new String ("Mario Kart 8", 60.0); //name of video game, price of video game
-        store[1] = new String ("Donkey Kong", 35.0);
-        store[2] = new String ("Pac-Man", 40.0);
-        store[3] = new String ("Splatoon 2", 55.0);
-        store[4] = new String ("Kirby's Dreamland", 30.0);
+    //Creating the setupStore method to return the array of games
+    public static Item[] setupStore () {
+        //creating the Item array for the setupStore method
+        Item[] store = new Item[5];
+        store[0] = new Item ("Mario Kart 8", 60.0); //name of video game, price of video game
+        store[1] = new Item ("Donkey Kong", 35.0);
+        store[2] = new Item ("Pac-Man", 40.0);
+        store[3] = new Item ("Splatoon 2", 55.0);
+        store[4] = new Item ("Kirby's Dreamland", 30.0);
         return store;
     }
 
